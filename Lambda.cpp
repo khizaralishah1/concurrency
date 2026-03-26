3d640abb6dd03ce59fdaab18998f2f46711a1ee2
khizaralishah1 <khizaralishahcs07@gmail.com>
2026-03-26 10:44:31 +0500

    Lambdas example. Add link to my notes

--- Lambda.cpp
+++ Lambda.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 
+// Implementation of lambda: Lambda is an anonymous class with operator()
+struct MyLambda {
+  int x;
+  int operator() (int y) const {
+    return x + y;
+  }
+};
 
-double SomeFunction() {
-  return 1;
-}
+
+double SomeFunction() { return 1; }
 
 int main() {
+  int x = 10;
 
+  auto default_lambda = [&]() {
+    std::cout << "Default lambda, x: " << x << std::endl;
+    return SomeFunction();
+  };
 
+  MyLambda my_lambda;
 
-  double lambda = [&]() { return SomeFunction(); }
+  default_lambda();
+
+  return EXIT_SUCCESS;
 }
